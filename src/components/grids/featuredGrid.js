70e82a075817464462c05fc2bb33592d63a49e79
Helene Syre <syrehelene@gmail.com>
2025-11-14 19:24:01 +0100

    Updates in footer and featured grid
    Changed 'Join us' to 'Join Us' in the footer heading for consistency. Also updated the featuredGrid component to use a <div> instead of a <section> as the grid container.

--- src/components/grids/featuredGrid.js
+++ src/components/grids/featuredGrid.js
@@ -2,7 +2,7 @@ import { getProductsByTags } from "../../utils/fetch.js";
 import { prouductCard } from "../cards/productCard.js";
 
 export async function featuredGrid() {
-    const grid = document.createElement('section');
+    const grid = document.createElement('div');
     grid.className = 'product__grid';
     const products = await getProductsByTags(['headphones', 'storage', 'electronics', 'audio', 'accessories', 'watches', 'watch', 'wearables', 'peripherals', 'gaming', 'computers']);
 
