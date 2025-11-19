11bb1b7d9c663290da42ce0712770c7d1cd01f69
Helene Syre <syrehelene@gmail.com>
2025-11-19 23:37:08 +0100

    Update featuredGrid.js

--- src/components/grids/featuredGrid.js
+++ src/components/grids/featuredGrid.js
@@ -6,7 +6,7 @@ export async function featuredGrid() {
     grid.className = 'product__grid';
     const products = await getProductsByTags(['headphones', 'storage', 'electronics', 'audio', 'accessories', 'watches', 'watch', 'wearables', 'peripherals', 'gaming', 'computers']);
 
-    products.forEach(product => {
+    products.slice(0, 12).forEach(product => {
         const card = prouductCard(product);
         grid.appendChild(card);
     });
